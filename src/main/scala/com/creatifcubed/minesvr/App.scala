17b8ac161485d1d6ae3585b52c2ec296337312e4
Raekye <adrian@creatifcubed.com>
2013-10-05 08:49:03 -0400

    removed scopt (dependency version)

--- src/main/scala/com/creatifcubed/minesvr/App.scala
+++ src/main/scala/com/creatifcubed/minesvr/App.scala
@@ -1,12 +1,47 @@
 package com.creatifcubed.minesvr;
 
-import java.io.File;
-
 /**
  * @author ${user.name}
  */
 object App {
+  val MINESVR_NAME = "Minesvr Client";
+  val MINESVR_VERSION = "0.0.1";
+  
   def main(args : Array[String]):Unit = {
-    print("Hello world!");
+    if (args.length < 1) {
+      help();
+    }
+  }
+  
+  def help(): Unit = {
+    version();
+    println("Usage:");
+    println("--version, -v");
+    println("--help, -h");
+    println("backup");
+    println("config <key <value>>");
+    println("push");
+    println("pull");
+    println("auto");
+  }
+  
+  def version(): Unit = {
+    println(MINESVR_NAME + " " + MINESVR_VERSION);
+  }
+  
+  def backup(): Unit = {
+    
+  }
+  
+  def push(): Unit = {
+    
+  }
+  
+  def pull(): Unit = {
+    
+  }
+  
+  def auto(): Unit = {
+    println("Use <ctrl> + c to quit");
   }
 }
